77f73f5b42c0675ca5927740d26b5b62170e0921
Sandro Gakharia <sandrogakharia@gmail.com>
2026-02-17 21:50:44 +0400

    Add GET and DELETE requests

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class TextRequest(BaseModel):
     
 @app.post("/analyze")
 def analyze_text(request: TextRequest):
+    if len(request.text) > 10000: raise fastapi.HTTPException(413, "Payload Too Large")
     with Session() as session:
         user_text = request.text
         count = len(user_text.split())
@@ -21,3 +22,20 @@ def analyze_text(request: TextRequest):
         session.refresh(new_entry)
         
         return new_entry
+    
+@app.get("/history")
+def get_history(limit: int = 10, offset: int = 0):
+    with Session() as session:
+        entries = session.query(models.TextEntry).limit(limit).offset(offset).all()
+        return entries
+    
+@app.delete("/id/{entry_id}")
+def delete_entry(entry_id: int):
+    with Session() as session:
+        entry = session.query(models.TextEntry).filter(models.TextEntry.id == entry_id).first()
+        if entry == None: raise fastapi.HTTPException(404, "Entry Not Found")
+        
+        session.delete(entry)
+        session.commit()
+        
+        return {"detail": "Entry deleted"}
